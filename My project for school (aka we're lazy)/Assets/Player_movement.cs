28364684989754d8a0a944e3bca616f36e45903f
Koby Wallers <kobywallers@gmail.com>
2023-05-27 21:10:49 +0200

    Dog animated now

--- My project for school (aka we're lazy)/Assets/Player_movement.cs
+++ My project for school (aka we're lazy)/Assets/Player_movement.cs
@@ -7,7 +7,8 @@ public class Player_movement : MonoBehaviour
     public float playerSpeed = 5f;
 
     public Rigidbody2D rb;
-
+    public Animator animator;
+    public float xAx, yAx;
     Vector2 movement;
 
     // Update is called once per frame
@@ -16,6 +17,19 @@ public class Player_movement : MonoBehaviour
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
 
+        if(movement.x != 0){
+            xAx = movement.x;
+            yAx = 0;
+        }
+        if(movement.y != 0){
+            yAx = movement.y;
+            xAx = 0;
+        }
+            
+        animator.SetFloat("Horizontal",xAx);
+        animator.SetFloat("Vertical",yAx);
+        animator.SetFloat("Speed",movement.sqrMagnitude);
+        
         movement.Normalize();
 
         rb.velocity = movement * playerSpeed;
